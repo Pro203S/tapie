71781926284f00813ab58be9f054edb8739e1af5
Pro203S <prosirea2501@gmail.com>
2026-05-27 16:14:34 +0900

    /health 추가

--- 0527_backend/index.js
+++ 0527_backend/index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("서버 정상 작동중");
 });
 
+app.get("/health", (req, res) => {
+    return res.status(200).json({ "status": "ok" });
+});
+
 app.listen(PORT, () => {
     console.log(`서버 실행 중: http://localhost:${PORT}`);
 });
