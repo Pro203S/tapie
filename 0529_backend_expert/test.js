798d87d1731dd4d96ca667631e93eb5f7d66210c
Pro203S <prosirea2501@gmail.com>
2026-05-29 16:52:25 +0900

    끝말잇기 만들기..?

--- 0529_backend_expert/test.js
+++ 0529_backend_expert/test.js
@@ -12,9 +12,29 @@ korean5800.txt에 있는 단어 중 랜덤한 1개를 출력
 */
 
 const fs = require("fs");
+const readline = require("readline-sync");
 
 const wordData = fs.readFileSync("./data/korean5800.txt", "utf-8").split(/\r?\n/);
+var targetWord = wordData;
+let randomIndex = Math.floor(Math.random() * wordData.length);
 
-const randomIndex = Math.floor(Math.random() * wordData.length);
+console.log("단어: " + targetWord[randomIndex]);
 
-console.log(wordData[randomIndex])
+for (; ;) {
+    let answer = readline.question("단어를 입력하세요: ");
+
+    if (answer[0] !== targetWord[randomIndex].slice(-1)) {
+        console.log("다른 단어를 입력해주세요.");
+        return;
+    }
+
+    if (!wordData.includes(answer)) {
+        console.log("사전에 없는 단어입니다.");
+        return;
+    }
+
+    const newWord = targetWord.filter(v => v.startsWith(answer[0]));
+    randomIndex = Math.floor(Math.random() * targetWord.length);
+
+    console.log("단어: " + targetWord[randomIndex]);
+}
