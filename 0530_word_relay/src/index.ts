ea039087ddbbfb91b35f3d43135b3351e44b4a66
Pro203S <prosirea2501@gmail.com>
2026-05-30 23:55:07 +0900

    asdfasd

--- 0530_word_relay/src/index.ts
+++ 0530_word_relay/src/index.ts
@@ -1,12 +1,13 @@
 import * as fs from 'fs';
-import { Client, Routes, type RESTPutAPIApplicationCommandsJSONBody } from 'discord.js';
+import { Client, Partials, Routes, type RESTPutAPIApplicationCommandsJSONBody } from 'discord.js';
 import dotenv from 'dotenv';
 import ora from 'ora';
 import 'colors';
 dotenv.config();
 
 const CLIENT = new Client({
-    "intents": 53608447
+    "intents": 53608447,
+    "partials": [Partials.Channel, Partials.Message]
 });
 
 const words = fs.readFileSync("./data/words.txt", "utf-8").split(/\r?\n/);
@@ -14,20 +15,20 @@ const getRandomWord = () => words[Math.floor(Math.random() * words.length)] as s
 
 const wordRelayState: Record<string, {
     "words": string[],
-    "lastWordFromUser"?: string,
-    "lastWordFromBot": string
+    "lastWordFromBot": string,
+    "timeout": NodeJS.Timeout
 }> = {};
 
-function toRgbHex(value: string): number {
-    const hex = value
-        .replace(/^#/, "")
-        .replace(/^0x/i, "");
+function toRgbHex(str: string): number {
+    let hash = 0;
 
-    if (!/^[0-9a-fA-F]{6}$/.test(hex)) {
-        throw new Error("Invalid color format");
+    for (let i = 0; i < str.length; i++) {
+
+        hash = ((hash << 5) - hash + str.charCodeAt(i)) | 0;
+
     }
 
-    return parseInt(hex, 16);
+    return hash & 0xffffff;
 }
 
 (async () => {
@@ -38,35 +39,79 @@ function toRgbHex(value: string): number {
     const REST = client.rest;
 
     await REST.put(Routes.applicationCommands(process.env.APP_ID), {
-        "body": [{
-            "name": "start",
-            "description": "끝말잇기를 시작해요."
-        }] satisfies RESTPutAPIApplicationCommandsJSONBody
+        "body": [
+            {
+                "name": "start",
+                "description": "끝말잇기를 시작해요."
+            },
+            {
+                "name": "stop",
+                "description": "끝말잇기를 끝내요."
+            }
+        ] satisfies RESTPutAPIApplicationCommandsJSONBody
     });
 
     spinner.succeed(`Logged in as ${`${client.user.username}#${client.user.discriminator}`.bold}`);
 
+    client.on("messageCreate", async (message) => {
+        const state = wordRelayState[message.author.id];
+        if (!state) return;
+
+        message.reply(JSON.stringify(state));
+    });
+
     client.on("interactionCreate", async (interaction) => {
         if (!interaction.isChatInputCommand()) return;
-        if (interaction.commandName !== "start") return;
+        if (interaction.commandName === "stop") {
+            return;
+        }
 
-        const word = getRandomWord();
+        if (interaction.commandName === "start") {
+            const word = getRandomWord();
 
-        wordRelayState[interaction.user.id] = {
-            "words": [],
-            "lastWordFromBot": getRandomWord()
-        };
+            wordRelayState[interaction.user.id] = {
+                "words": [],
+                "lastWordFromBot": word,
+                "timeout": setTimeout(async () => {
+                    delete wordRelayState[interaction.user.id];
+                    await interaction.editReply({
+                        "embeds": [{
+                            "color": 0xff1414,
+                            "title": "⏰ 시간이 초과되었어요.",
+                            "description": "5초 안에 단어를 내지 못해 게임이 종료되었어요.",
+                            "footer": {
+                                "icon_url": "https://cdn.discordapp.com/avatars/388653325161332736/a_7f08fd9a1098df2178a9a4d84a185055.webp?size=32",
+                                "text": "Made by. @pro203s"
+                            }
+                        }]
+                    });
+                    return;
+                }, 5000)
+            };
+
+            return await interaction.reply({
+                "embeds": [{
+                    "color": toRgbHex(word),
+                    "title": "✏️ 끝말잇기 시작!",
+                    "description": `저 먼저 시작할게요.\n이 채팅에 단어의 끝 글자로 시작하는 단어를 보내주세요.\n**시간 제한은 5초입니다.**\n\n### ${word}!`,
+                    "footer": {
+                        "icon_url": "https://cdn.discordapp.com/avatars/388653325161332736/a_7f08fd9a1098df2178a9a4d84a185055.webp?size=32",
+                        "text": "Made by. @pro203s"
+                    }
+                }]
+            });
+        }
 
         return await interaction.reply({
             "embeds": [{
-                "color": toRgbHex(getRandomWord()),
-                "title": "✏️ 끝말잇기를 시작할게요",
-                "description": `저 먼저 시작할게요.\n\n**${word}!**`,
+                "color": 0xff1414,
+                "title": "🤕 알 수 없는 명령어에요.",
+                "description": "알 수 없는 명령어에요.\n사용 가능한 명령어는 /start, /stop이 있어요.",
                 "footer": {
-                    // "icon_url",
+                    "icon_url": "https://cdn.discordapp.com/avatars/388653325161332736/a_7f08fd9a1098df2178a9a4d84a185055.webp?size=32",
                     "text": "Made by. @pro203s"
                 }
             }]
-        })
+        });
     });
 })();
